9f029035bce80b85c6acde25d3f66dcd1fec39eb
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-10-19 15:40:08 +0100

    comir

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
             <p>
                 <?php
                     echo "hello world";
+                    echo "Hello, world!";
+                    echo "Hello," . "" . "world" . "!";
+                    echo 5*7;
+                    $myname = "Frodo Baggins";
+                    &myage = 111;
+                    echo "My name is" . $myname . "and i am" . $myage
                 ?>
             </p>
         </body>
