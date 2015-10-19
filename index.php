766674ebd4a040f8fe57d5d08cb22b623d572614
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-10-19 15:52:48 +0100

    comir

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 <body>
 <p>
     <?php
+    $name = "Edgar";
+
+    if ($name == "Simon") {
+        print "I know you!";
+    }
+    else {
+    print "Who are you?";
+
     echo "hello world";
     echo "Hello, world!";
     echo "Hello," . "" . "world" . "!";
@@ -12,6 +20,7 @@
     $myname = "Frodo Baggins";
     $myage = 111;
     echo "My name is" . $myname . "and i am" . $myage
+
     ?>
 </p>
 </body>
