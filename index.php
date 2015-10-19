d2f077f464b5cb29cef246b771f4ee9a5e7021f4
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-10-19 15:22:13 +0100

    comir

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <body>
+            <p>
+                <?php
+                    echo "hello world";
+                ?>
+            </p>
+        </body>
+    </head>
+</html>
