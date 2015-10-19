53992f8cad2304ec83d12d8bff0857b0eac79682
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-10-19 16:56:18 +0100

    comir

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     $myage = 111;
     echo "My name is" . $myname . "and i am" . $myage
     */
-    $myage = 20;
+    /*$myage = 20;
     echo "<p>i am " . $myage . " years old</p>";
     if ($myage >= 21) {
         echo "<p>you can buy sausage rolls</p>";
@@ -34,6 +34,22 @@
     }
     else {
         echo "<p>you can buy nothing</p>";
+    }
+    */
+    $wantedgood = "mugs";
+
+    switch ($wantedgood){
+        case "specs":
+            echo "You have to be 16 to buy $wantedgood";
+            break;
+        case "mugs":
+            echo "You have to be 18 to buy $wantedgood";
+            break;
+        case "sausage rolls":
+            echo "You have to be 21 to buy $wantedgood";
+            break;
+    }
+    ?>
     }
 
     ?>
