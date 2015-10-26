0491889cb22602e80118c575ac89ee2863f2cde6
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-10-26 15:44:44 +0000

    comir

---
+++ helloSelector.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello selector</title>
+</head>
+<body>
+Where would you like to say hello
+
+<a href="helloPrinter.php?planet=Earth">Earth</a>
+
+
+</body>
+</html>
