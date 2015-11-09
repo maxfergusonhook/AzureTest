fce19789e68aa6ac18e7a36d12bcb1c2ae25455b
1308886 <m.ferguson-hook@rgu.ac.uk>
2015-11-09 15:14:43 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -4,11 +4,9 @@
 
 $db = new mysqli("br-cdbr-azure-south-a.cloudapp.net","bcc549c1bd791c","acf8b795", "azuretest");
 
-// test if connection was established, and print any errors
-
 // create a SQL query as a string
 
-$sql_query = "SELECT * FROM superheros ?WHERE productionStudio LIKE '%Fox%'";
+$sql_query = "SELECT * FROM marvelmovies ?WHERE productionStudio LIKE '%Fox%'";
 
 // execute the SQL query
 
@@ -16,7 +14,7 @@ $result = $db->query($sql_query);
 
 // iterate over $result object one $row at a time
 // use fetch_array() to return an associative array
-
+echo "<h2>List of Movies</h2>";
 while($row = $result->fetch_array()){
    // print out fields from row of data
    echo "<p>" . $row['title'] . "</p>";
