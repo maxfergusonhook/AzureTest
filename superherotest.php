c5e176c3188fe0d96b8d4880599ba7745505f3a1
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 12:20:41 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,11 +1,17 @@
 <?php
-   function do_something(&$arg)
-   {
-      $retval = $arg;
-      $arg = $arg + 1;
-      return $retval;
-   }
-   $a = 3;
-   $b = do_something($a);
-   echo $a, "", $b;
+$numberOfHobbits = 2;
+
+switch ($numberOfHobbits) {
+   case 1:
+      echo "1 sad hobbit";
+      break;
+   case 2:
+      echo "2 happy hobbits";
+      break;
+   case 3:
+      echo "3 hobbits are a crowd";
+      break;
+   default:
+      echo "All the hobbits have gone home";
+
 ?>
