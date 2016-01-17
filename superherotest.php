c9e82e987411f2c9d2fb17cdcbcc57f5452ef7c2
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 12:30:52 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,18 +1,28 @@
 <?php
-   $numberOfHobbits = 2;
+for ($i = 1; $i < 30; $i++)
+{
+   $y = 0; //I'm using this as a marker to tell that at least one thing is being made
+   echo "<p>On day " . $i . " the following products are available: ";
 
-   switch ($numberOfHobbits) {
-      case 1:
-         echo "1 sad hobbit";
-         break;
-      case 2:
-         echo "2 happy hobbits";
-         break;
-      case 3:
-         echo "3 hobbits are a crowd";
-         break;
-      default:
-         echo "All the hobbits have gone home";
+   if ($i % 2 != 0)
+   {
+      echo "Sausage Rolls ";
+      $y = 1;
    }
+   if ($i % 3 != 0)
+   {
+      echo "Mugs ";
+      $y = 1;
+   }
+   if ($i % 4 != 0)
+   {
+      echo "Specs";
+      $y = 1;
+   }
+   if ($y == 1) //if one thing isn't being made on this day then it displays that nothing is available that day.
+   {
+      echo "NONE";
+   }
+   echo "</p>";
 
 ?>
