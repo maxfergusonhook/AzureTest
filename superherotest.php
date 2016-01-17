935fff6b1f97d22c01e882ff9e56239cfa8af94a
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 10:54:10 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,10 +1,11 @@
-function do_something(&$arg)
-{
-   $retval = $arg;
-   $arg = $arg;
-   return $retval;
-}
-$a = 3
-$b = do_something($a);
-echo $a, " ", $b;
-
+<?php
+   function do_something(&$arg)
+   {
+      $retval = $arg;
+      $arg = $arg + 1;
+      return $retval;
+   }
+   $a = 3;
+   $b = do_something($a);
+   echo $a, "", $b;
+?>
