14e2d303ebe7c1e64d874568fd273255a9595e42
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 10:29:31 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,21 +1,5 @@
 <?php
-
-// connect to your server and select database
-
-$db = new mysqli("br-cdbr-azure-south-a.cloudapp.net","bcc549c1bd791c","acf8b795", "azuretest");
-
-// create a SQL query as a string
-
-$sql_query = "SELECT * FROM marvelmovies";
+   echo "hello";
+?>
 
 
-$result = $db->query($sql_query);
-
-echo "<h2>List of Movies</h2>";
-
-while($row = $result->fetch_array()){
-   echo "<p>" . $row['title'] . " was released in <strong>" . $row['yearReleased'] . "</strong></p>";
-}
-
-$result->close();
-   $db->close();
