45e94ab25df5bdeb5f8a8f6e1219b9ed84e95445
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 12:23:10 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,17 +1,18 @@
 <?php
-$numberOfHobbits = 2;
+   $numberOfHobbits = 2;
 
-switch ($numberOfHobbits) {
-   case 1:
-      echo "1 sad hobbit";
-      break;
-   case 2:
-      echo "2 happy hobbits";
-      break;
-   case 3:
-      echo "3 hobbits are a crowd";
-      break;
-   default:
-      echo "All the hobbits have gone home";
+   switch ($numberOfHobbits) {
+      case 1:
+         echo "1 sad hobbit";
+         break;
+      case 2:
+         echo "2 happy hobbits";
+         break;
+      case 3:
+         echo "3 hobbits are a crowd";
+         break;
+      default:
+         echo "All the hobbits have gone home";
+   }
 
 ?>
