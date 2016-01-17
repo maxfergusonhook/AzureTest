8ab85741c068fd483f5a2554eb7dd32d71a8ce0a
1308886 <m.ferguson-hook@rgu.ac.uk>
2016-01-17 14:18:10 +0000

    comir

--- superherotest.php
+++ superherotest.php
@@ -1,26 +1,13 @@
 <?php
-   $lotteryArray = array("Mike", "John", "Mark", "Angela", "Roger", "Chrisina", "Suzanne", "Sue" , "Ines", "Ian");
-
-   //Sort the array
-   sort($lotteryArray);
-
-   //Select the winner
-   $winner = rand(0,count($lotteryArray));
-
-   //Prints the winner
-   echo "<p>The winner of all the specs is " . strtoupper($lotteryArray[$winner]) . "</p>";
-
-   //Remove the person from the array
-   unset($array[$winner]);
-
-   //Do the same again for mugs
-   $winner = rand(0,count($lotteryArray));
-   echo "<p>The winner of all the mugs is " . strtoupper($lotteryArray[$winner]) . "</p>";
-   unset($array[$winner]);
-
-   //and the sausuage rolls
-   $winner = rand(0,count($lotteryArray));
-   echo "<p>The winner of all the sausage rolls is " . strtoupper($lotteryArray[$winner]) . "</p>";
+   function do_something(&$arg)
+   {
+      $retval = $arg;
+      $arg = $arg + 1;
+      return $retval;
+   }
+   $a = 3;
+   $b = do_something($a);
+   echo $a, " ", $b;
 
 ?>
 
